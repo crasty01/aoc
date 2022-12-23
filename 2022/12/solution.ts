114c378fa6e770937bd6023379363af091812e7c
Crasty <35274555+crasty01@users.noreply.github.com>
2022-12-24 00:24:19 +0100

    day 2022 12

---
+++ 2022/12/solution.ts
@@ -0,0 +1,73 @@
+type Pos = {x: number, y: number};
+type Input = [Pos, Pos, Array<Array<number>>];
+
+export const getCellHeight = (cell: string): number => {
+  if (cell === 'S') return 1;
+  if (cell === 'E') return 26;
+  return cell.charCodeAt(0) - 96;
+}
+
+export const parseInput = (rawInut: string): Input => {
+  let start: Pos | undefined;
+  let end: Pos | undefined;
+
+  const grid = rawInut.split('\r\n').map((line, index) => {
+    const row = [] as Array<number>;
+    for (let i = 0; i < line.length; i++) {
+      if (line[i] === 'S') start = {x: i, y: index};
+      if (line[i] === 'E') end = {x: i, y: index};
+      row.push(getCellHeight(line[i]));
+    }
+    return row;
+  });
+
+  if (!start || !end) throw new Error('No start or end found');
+  return [start, end, grid];
+}
+
+const getShortedstPath = (start: Pos, end: Pos, grid: Array<Array<number>>): number => {  
+  const queue = [{ ...start, steps: 0 }];
+  const visited = new Set<string>();
+
+  while (queue.length > 0) {
+    const current = queue.shift()!;
+    if (current.x === end.x && current.y === end.y) return current.steps;
+    const next = [
+      { x: current.x - 1, y: current.y },
+      { x: current.x + 1, y: current.y },
+      { x: current.x, y: current.y - 1 },
+      { x: current.x, y: current.y + 1 },
+    ];
+    for (const pos of next) {
+      if (pos.x < 0 || pos.x >= grid[0].length || pos.y < 0 || pos.y >= grid.length) continue;
+      if (visited.has(`${pos.x},${pos.y}`)) continue;
+      if (grid[pos.y][pos.x] - grid[current.y][current.x] > 1) continue;
+      visited.add(`${pos.x},${pos.y}`);
+      queue.push({ ...pos, steps: current.steps + 1 });
+    }
+  }
+
+  throw new Error("No solution found");
+}
+
+export const solution1 = ([start, end, grid]: Input): number | string =>  {
+  return getShortedstPath(start, end, grid);
+}
+
+export const solution2 = ([_, end, grid]: Input): number | string =>  {
+  let shortestPath = Number.MAX_SAFE_INTEGER;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[0].length; x++) {
+      if (grid[y][x] === 1) {
+        try {
+          const path = getShortedstPath({ x, y }, end, grid);
+          if (path < shortestPath) shortestPath = path;
+        } catch (e) {
+          // no path found
+        }
+      }
+    }
+  }
+
+  return shortestPath;
+}
