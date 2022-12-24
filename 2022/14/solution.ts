043fdda960bd1fae6863077754478ee7b28b6512
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2022-12-24 01:45:22 +0100

    created input

---
+++ 2022/14/solution.ts
@@ -0,0 +1,13 @@
+type Input = any;
+
+export const parseInput = (rawInut: string): Input => {
+  return rawInut;
+}
+
+export const solution1 = (input: Input): number | string =>  {
+  return 0;
+}
+
+export const solution2 = (input: Input): number | string =>  {
+  return 0;
+}
