8d7dd1d6fdb7daced50ea9b99f57cf69702f6af8
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2022-12-29 21:53:16 +0100

    add run: boolean

--- solution.template.ts
+++ solution.template.ts
@@ -4,10 +4,12 @@ export const parseInput = (rawInut: string): Input => {
   return rawInut;
 }
 
-export const solution1 = (input: Input): number | string =>  {
+export const solution1 = (input: Input, run = false): number =>  {
+  if (!run) return -1;
   return 0;
 }
 
-export const solution2 = (input: Input): number | string =>  {
+export const solution2 = (input: Input, run = false): number =>  {
+  if (!run) return -1;
   return 0;
 }
