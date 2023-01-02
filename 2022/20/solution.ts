bf8746e97f52872770ef46c87228205e6099b19c
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2023-01-02 09:37:15 +0100

    part 1

--- 2022/20/solution.ts
+++ 2022/20/solution.ts
@@ -1,19 +1,64 @@
 type Input = Array<number>;
 
 export const parseInput = (rawInut: string): Input => {
-  return rawInut.split('\r\n').map((line) => parseInt(line, 10));
+  return rawInut.split('\r\n').map((line) =>  parseInt(line, 10));
 }
 
-export const solution1 = (input: Input, run = false): number =>  {
-  if (!run) return -1;
-  return 0;
+const getNewIndex = (currentIndex: number, value: number, length: number): number => {
+  if (value === 0) return currentIndex;
+  switch (Math.sign(value)) {
+    case -1: return (currentIndex + value + length) % length;
+    case 0: return currentIndex;
+    case 1: return (currentIndex + value + length) % length;
+    default: throw new Error('Invalid value');
+  }
+}
+
+const mix = (input: Input): Array<number> => {
+  const array = input.map((value) => [value, false]) as Array<[number, boolean]>;
+  const n = array.length;
+  let currentIndex = 0;
+
+  for (let i = 0; i < n; i++) {
+    const [value] = array.splice(currentIndex, 1)[0];
+    const newIndex = getNewIndex(currentIndex, value, array.length)
+
+    // console.log(`Move ${value} from ${currentIndex} to ${newIndex}`);
+    // console.log({ currentIndex, value, newIndex });
+    array.splice(newIndex, 0, [value, true]);
+    currentIndex = array.findIndex((line) => line[1] === false);
+    // console.log(input.map((line) => line[0]));
+  }
+
+  return array.map((line) => line[0]);
+}
+
+const getSum = (input: Array<number>): number => {
+  const zeroIndex = input.findIndex((line) => line === 0);
+  const a = getNewIndex(zeroIndex, 1000, input.length);
+  const b = getNewIndex(a, 1000, input.length);
+  const c = getNewIndex(b, 1000, input.length);
+  console.log(input[a], input[b], input[c]);
+  return input[a] + input[b] + input[c];
+}
+
+export const solution1 = (input: Input): number =>  {
+  const mixed = mix(input);
+  return getSum(mixed);
 }
 
 export const solution2 = (input: Input, run = false): number =>  {
   if (!run) return -1;
-  return 0;
+  let mixed = input.map((value) => value * 811589153);
+  console.log(mixed);
+  
+  for (let i = 0; i < 10; i++) {
+    mixed = mix(mixed);
+    console.log(mixed);
+  }
+  return getSum(mixed);
 }
 
 const example = `1\r\n2\r\n-3\r\n3\r\n-2\r\n0\r\n4`;
-console.log('Example 1:', solution1(parseInput(example), true));
-// console.log('Example 2:', solution2(parseInput(example), true));
+// console.log('Example 1:', solution1(parseInput(example)));
+console.log('Example 2:', solution2(parseInput(example), true));
