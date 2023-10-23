c8174dec0c906b76240778205f58f7aedbf641eb
Crasty <danvondra01@gmail.com>
2023-10-23 18:16:06 +0200

    v2 (#12)

--- 2022/06/solution.ts
+++ 2022/06/solution.ts
@@ -4,24 +4,32 @@ export const parseInput = (input: string): string => {
   return input;
 };
 
-const unique = (input: string): boolean => input.length === new Set([...input]).size;
+const countOnes = (n: number) => {
+  let count = 0;
+  while (n >= 1) { count += n & 1; n = n >> 1; }
+  return count;
+};
 
-export const solution1 = (input: Input): number | string =>  {
-  const CHARS = 4;
-  let last = input.slice(0, CHARS - 1);
-  for (let i = CHARS - 1; i < input.length; i++) {
-    last = last.slice(-1 * (CHARS - 1)) + input[i];
-    if (unique(last)) return i + 1;
+const binarySolution = (input: Input, CHARS: number): number | string => {
+  let filter = 0;
+  for (let i = 0; i < CHARS - 1; i++) {
+    const c = input[i].charCodeAt(0);
+    filter ^= 1 << (c % 32);
+  }
+  for (let i = 0; i < input.length - CHARS - 1; i++) {
+    const f = input[i].charCodeAt(0);
+    const l = input[i + CHARS - 1].charCodeAt(0);
+    filter ^= 1 << (l % 32);
+    if (countOnes(filter) === CHARS) return i + CHARS;
+    filter ^= 1 << (f % 32);
   }
   return -1;
 }
 
+export const solution1 = (input: Input): number | string =>  {
+  return binarySolution(input, 4)
+}
+
 export const solution2 = (input: Input): number | string =>  {
-  const CHARS = 14;
-  let last = input.slice(0, CHARS - 1);
-  for (let i = CHARS - 1; i < input.length; i++) {
-    last = last.slice(-1 * (CHARS - 1)) + input[i];
-    if (unique(last)) return i + 1;
-  }
-  return -1;
+  return binarySolution(input, 14)
 }
