90682e2218c46b2ebdd056c217bddce20c78c705
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2023-10-23 18:14:51 +0200

    v2

---
+++ 2022/06/solutions.old.ts
@@ -0,0 +1,24 @@
+type Input = string;
+
+export const simpleSolution = (input: Input, CHARS: number): number | string =>  {
+  const unique = (input: string): boolean => input.length === new Set([...input]).size;
+
+  let last = input.slice(0, CHARS - 1);
+  for (let i = CHARS - 1; i < input.length; i++) {
+    last = last.slice(-1 * (CHARS - 1)) + input[i];
+    if (unique(last)) return i + 1;
+  }
+  return -1;
+}
+
+export const fasterSolution = (input: Input, CHARS: number): number | string =>  {
+  for (let i = CHARS - 1; i < input.length; i++) {
+    const set = new Set();
+    for (let j = 0; j < CHARS; j++) {
+      if (set.has(input[i - j])) break;
+      set.add(input[i - j]);
+    }
+    if (set.size === CHARS) return i + 1;
+  }
+  return -1;
+}
