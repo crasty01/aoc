5761c8ce4bb6018b2adb7d874d9d0c067ddaf7c4
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2023-12-12 22:05:54 +0100

    year 2023 days 10,11

---
+++ 2023/10/solution.ts
@@ -0,0 +1,169 @@
+type Input = {
+	width: number;
+	height: number;
+	content: string;
+	start: number;
+};
+
+type Part = {
+	symbol: string;
+	pos: number;
+}
+
+export const parseInput = (rawInut: string): Input => {
+	const width = rawInut.indexOf('\r\n') > -1 ? rawInut.indexOf('\r\n') : rawInut.indexOf('\n');
+  const content =  rawInut.replace(/\r?\n/g, '');
+	const start = content.indexOf('S');
+
+	return {
+		width,
+		height: content.length / width,
+		content,
+		start,
+	}
+}
+
+const getPositionBySymbol = (pos: number, width: number, symbol: string): Array<number> => {
+	const top = pos - width;
+	const right = pos + 1;
+	const bottom = pos + width;
+	const left = pos - 1;
+	switch (symbol) {
+		case '|': return [top, bottom]
+		case '-': return [left, right]
+		case 'L': return [top, right]
+		case 'J': return [top, left]
+		case '7': return [left, bottom]
+		case 'F': return [right, bottom]
+		case '.': return []
+		case 'S': return []
+		default: throw new Error(`UNKNOWN SYMBOL: '${symbol}'`);
+	}
+}
+
+export const solution1 = (input: Input): number =>  {
+	const path: Array<Part> = [{
+		symbol: 'S',
+		pos: input.start,
+	}];
+
+	loop: for (let i = -1; i <= 1; i++) {
+		for (let j = -1; j <= 1; j++) {
+			if (i === 0 && j === 0) continue;
+			const pos = path[0].pos + i * input.width + j;
+			const possibleStartPositions = getPositionBySymbol(pos, input.width, input.content[pos]);
+			const possibleStartIndex = possibleStartPositions.indexOf(path[0].pos)
+			if (possibleStartIndex !== -1) {
+				path.push({
+					pos,
+					symbol: input.content[pos]
+				});
+				break loop;
+			}
+		}
+	}
+
+	while (path.at(-1)!.symbol !== 'S') {
+		const prev = path.at(-2)!;
+		const current = path.at(-1)!;
+		const nextPositions = getPositionBySymbol(current.pos, input.width, current.symbol);
+		const nextPosition = nextPositions.find(pos => pos !== prev.pos);
+		if (!nextPosition) throw new Error("NO NEXT POSITION");
+		
+		path.push({
+			pos: nextPosition,
+			symbol: input.content[nextPosition]
+		});
+	}
+	
+  return Math.floor(path.length / 2);
+}
+
+export const solution2 = (input: Input): number =>  {
+	const path: Array<Part> = [{
+		symbol: 'S',
+		pos: input.start,
+	}];
+
+	loop: for (let i = -1; i <= 1; i++) {
+		for (let j = -1; j <= 1; j++) {
+			if (i === 0 && j === 0) continue;
+			const pos = path[0].pos + i * input.width + j;
+			const possibleStartPositions = getPositionBySymbol(pos, input.width, input.content[pos]);
+			const possibleStartIndex = possibleStartPositions.indexOf(path[0].pos)
+			if (possibleStartIndex !== -1) {
+				path.push({
+					pos,
+					symbol: input.content[pos]
+				});
+				break loop;
+			}
+		}
+	}
+
+	while (path.at(-1)!.symbol !== 'S') {
+		const prev = path.at(-2)!;
+		const current = path.at(-1)!;
+		const nextPositions = getPositionBySymbol(current.pos, input.width, current.symbol);
+		const nextPosition = nextPositions.find(pos => pos !== prev.pos);
+		if (!nextPosition) throw new Error("NO NEXT POSITION");
+	
+		if (input.content[nextPosition] === 'S') break;
+		path.push({
+			pos: nextPosition,
+			symbol: input.content[nextPosition]
+		});
+	}
+
+	const firstAndLast = `${path[1].symbol}${path.at(-1)!.symbol}`;
+	switch (firstAndLast) {
+		case 'J|':
+			path[0].symbol = 'F'
+			break;
+		case '||':
+			path[0].symbol = '|'
+			break;
+		default: throw new Error(`CANNOT GUESS STARTING SYMBOL FOR: '${firstAndLast}'`);
+	}
+	// TODO: add all possible guesses
+
+	const map = new Map<number, number>();
+	for (let i = 0; i < path.length; i++) {
+		map.set(path[i].pos, i);
+	}
+
+	let inside = 0;
+	let isInside = false;
+	let lastBend: undefined | Part = undefined;
+	for (let pos = 0; pos < input.content.length; pos++) {
+		if (pos % input.width === 0) isInside = false;
+
+		if (!map.has(pos)) {	
+			inside += +isInside;
+			continue;
+		}
+		
+		const part = path[map.get(pos)!];
+
+		if (part.symbol === '-') continue;
+		if (part.symbol === '|') {
+			isInside = !isInside;
+			continue;
+		}
+		if (!lastBend) {
+			lastBend = part;
+			continue;
+		}
+
+		if (
+			(lastBend.symbol === 'F' && part.symbol === 'J') ||
+			(lastBend.symbol === 'L' && part.symbol === '7')
+		) {
+			isInside = !isInside
+		}
+
+		lastBend = undefined;
+	}
+	
+  return inside;
+}
---
+++ 2023/11/solution.ts
@@ -0,0 +1,79 @@
+type Galaxy = {
+	x: number;
+	y: number;
+}
+type Input = {
+	content: string,
+	width: number,
+	height: number,
+	galaxies: Array<Galaxy>
+	blanks: {
+		x: Array<number>;
+		y: Array<number>;
+	},
+};
+
+export const parseInput = (rawInut: string): Input => {
+	const content = rawInut.replace(/\r?\n/g, '');
+	const width = rawInut.replace('\r\n', '\n').indexOf('\n');
+	const height = content.length / width;
+	const galaxies: Array<Galaxy> = [];
+	const blanks = {
+		x: Array.from({ length: width }, () => true),
+		y: Array.from({ length: height }, () => true),
+	}
+	for (let i = 0; i < content.length; i++) {
+		if (content[i] !== '#') continue;
+		galaxies.push({
+			x: i % width,
+			y: Math.floor(i / width),
+		});
+		blanks.x[i % width] = false;
+		blanks.y[Math.floor(i / width)] = false;
+	}
+
+  return {
+		content,
+		width,
+		height,
+		blanks: {
+			x: [...blanks.x.entries()].filter(e => e[1]).map(e => e[0]),
+			y: [...blanks.y.entries()].filter(e => e[1]).map(e => e[0]),
+		},
+		galaxies,
+	};
+}
+
+const solution = (input: Input, expansion: number): number =>  {
+	let sum = 0;
+
+	for (let i = 0; i < input.galaxies.length - 1; i++) {
+		for (let j = i + 1; j < input.galaxies.length; j++) {
+			const a = input.galaxies[i];
+			const b = input.galaxies[j];
+
+			const blankX = input.blanks.x.filter(e => a.x > b.x
+				? e > b.x && e < a.x
+				: e > a.x && e < b.x
+			).length;
+			const blankY = input.blanks.y.filter(e => a.y > b.y
+				? e >= b.y && e <= a.y
+				: e >= a.y && e <= b.y
+			).length;
+
+			const baseDistance = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+			const blankDistance = (blankX + blankY) * (expansion - 1);
+			sum += baseDistance + blankDistance;
+		}
+	}
+
+  return sum;
+}
+
+export const solution1 = (input: Input): number =>  {
+  return solution(input, 2);
+}
+
+export const solution2 = (input: Input, run = false): number =>  {
+	return solution(input, 1_000_000);
+}
