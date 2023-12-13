158cfe342714c9e9c69d08c6db94928800711039
Daniel Vondra <35274555+crasty01@users.noreply.github.com>
2023-12-13 23:15:21 +0100

    year 2023 days 12,13

---
+++ 2023/12/solution.ts
@@ -0,0 +1,70 @@
+// inspired by https://github.com/hyper-neutrino/advent-of-code/blob/main/2023/day12p2.py
+
+type Input = Array<{
+	conditions: string;
+	sizes: Array<number>;
+}>;
+
+export const parseInput = (rawInut: string): Input => {
+  return rawInut.split(/\r?\n/g).map(line => {
+		const [conditions, sizesData] = line.split(' ');
+		const sizes = sizesData.split(',').map(e => parseInt(e, 10));
+
+		return { conditions, sizes }
+	})
+};
+
+const findArrangements = (
+  conditions: string,
+	sizes: Array<number>,
+  cache = new Map<string, number>(),
+): number => {
+  const key = `${conditions}-${sizes}`;
+  if (cache.has(key)) return cache.get(key)!;
+  if (conditions === '') return sizes.length ? 0 : 1;
+  if (sizes.length === 0) return conditions.includes('#') ? 0 : 1;
+
+  let result = 0;
+
+  const condition = conditions[0];
+  if (condition === '.' || condition === '?') {
+    result += findArrangements(conditions.slice(1), sizes, cache);
+  }
+
+  const first = sizes[0];
+  const rest = sizes.slice(1);
+
+	if (true
+		&& (condition === '#' || condition === '?')
+		&& first <= conditions.length
+		&& !conditions.slice(0, first).includes('.')
+		&& (first === conditions.length || conditions[first] !== '#')
+	) {
+		result += findArrangements(conditions.slice(first + 1), rest, cache);
+	}
+
+  cache.set(key, result);
+  return result;
+};
+
+export const solution1 = (input: Input): number => {
+	let sum = 0;
+
+	for (const line of input) {
+		sum += findArrangements(line.conditions, line.sizes);
+	}
+
+  return sum;
+};
+
+export const solution2 = (input: Input): number => {
+	let sum = 0;
+
+	for (const line of input) {
+		const conditions = (line.conditions + '?').repeat(5).slice(0, -1);
+		const sizes = Array.from({ length: line.sizes.length * 5 }, (_, i) => line.sizes[i % line.sizes.length]);
+		sum += findArrangements(conditions, sizes);
+	}
+
+  return sum;
+};
---
+++ 2023/13/solution.ts
@@ -0,0 +1,72 @@
+type Image = Array<string>;
+type Input = Array<Image>;
+
+export const parseInput = (rawInut: string): Input => {
+	return rawInut.split(/\r?\n\r?\n/g).map(image => image.split(/\r?\n/g));
+}
+
+const compareHorizontally = (image: Image, exactNumberOfMistakes = 0): number => {
+	const width = image[0].length;
+	const height = image.length;
+
+	for (let line = 1; line <= width - 1; line++) {
+		let mistakes = 0;
+		const offset = Math.min(line, width - line);
+		 for (let column = line - offset; column < line ; column++) {
+			const mirrored = 2 * line - column - 1;
+			
+			for (let y = 0; y < height; y++) {
+				if (image[y][column] !== image[y][mirrored]) {
+					mistakes += 1;
+				}
+			}
+		}
+		if (mistakes === exactNumberOfMistakes) return line;
+	}
+
+	return 0;
+}
+
+const compareVertically = (image: Image, exactNumberOfMistakes = 0): number => {
+	const width = image[0].length;
+	const height = image.length;
+
+	for (let line = 1; line <= height - 1; line++) {
+		let mistakes = 0;
+		const offset = Math.min(line, height - line);
+		 for (let row = line - offset; row < line; row++) {
+			const mirrored = 2 * line - row - 1;
+			
+			for (let x = 0; x < width; x++) {
+				if (image[row][x] !== image[mirrored][x]) {
+					mistakes += 1;
+				}
+			}
+		}
+		if (mistakes === exactNumberOfMistakes) return line;
+	}
+
+	return 0;
+}
+
+export const solution1 = (input: Input): number =>  {
+	let sum = 0;
+
+	for (const image of input) {
+		sum += compareHorizontally(image);
+		sum += compareVertically(image) * 100;
+	}
+
+  return sum;
+}
+
+export const solution2 = (input: Input): number =>  {
+	let sum = 0;
+
+	for (const image of input) {
+		sum += compareHorizontally(image, 1);
+		sum += compareVertically(image, 1) * 100;
+	}
+
+  return sum;
+}
